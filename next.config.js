e2aeb491841b23ed764db7814a74cc4f37717861
Anshul Rana <50067136+anshulranaa@users.noreply.github.com>
2024-07-05 13:44:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,36 +1,26 @@
-// next.config.js
-const path = require('path');
+/** @type {import('next').NextConfig} */
+const withLess = require('next-with-less')
+const path = require('node:path')
 
-module.exports = {
+const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   swcMinify: true,
-  transpileModules: ['antd-mobile'], // Note: Changed to transpileModules
-  webpack: (config, { isServer }) => {
-    // Modify webpack config here
+  transpilePackages: ['antd-mobile'],
+  webpack(config, options) {
+    // disable css-module in Next.js
     config.module.rules.forEach((rule) => {
-      const { oneOf } = rule;
+      const { oneOf } = rule
       if (oneOf) {
         oneOf.forEach((one) => {
-          if (!`${one.issuer?.and}`.includes('_app')) return;
-          one.issuer.and = [path.resolve(__dirname)];
-        });
+          if (!`${one.issuer?.and}`.includes('_app')) return
+          one.issuer.and = [path.resolve(__dirname)]
+        })
       }
-    });
+    })
 
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        // Matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
-  },
-};
+    return config
+  }
+}
+
+module.exports = withLess(nextConfig)
